cee3ca5479cad62e1dbbbf782190b55cfe7dc320
chrisalee <christopher.a.lee5@gmail.com>
2017-12-09 21:18:45 -0500

    Change hardcoded data to dynamically fetched data

--- pages/index.js
+++ pages/index.js
@@ -6,20 +6,12 @@ import Company from '../components/company.js';
 
 class Index extends React.Component {
     static async getInitialProps() {
-        // const res = await fetch('');
-        // const data = await res.json();
-        // console.log(`Data fetched. Count: ${data.length}`);
-        //
-
-        const companies = [
-            {company: {id: 1, status: true, name: 'facebook'}},
-            {company: {id: 2, status: false, name: 'amazon'}},
-            {company: {id: 3, status: true, name: 'microsoft'}},
-            {company: {id: 4, status: false, name: 'google'}},
-        ];
+        const res = await fetch('http://localhost:4000/db');
+        const data = await res.json(res);
+        console.log(`Data fetched. Count: ${data.results}`);
 
         return {
-            companies,
+            companies: data.results,
         };
     }
 
@@ -39,7 +31,7 @@ class Index extends React.Component {
                 <button onClick={this.toggleOpen.bind(this)}>Open</button>
                 <p>Hello World</p>
                 <ul>
-                    {this.props.companies.map(({company}) => {
+                    {this.props.companies.map(company => {
                         if (!company.status && !this.state.open) {
                             return;
                         }
