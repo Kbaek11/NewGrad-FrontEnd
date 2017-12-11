936280139720014fa6ea7737383833a631701b43
chrisalee <christopher.a.lee5@gmail.com>
2017-12-10 21:23:23 -0500

    Fix button to display companies

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,9 @@ import Head from '../components/head.js';
 
 class Index extends React.Component {
     static async getInitialProps() {
-        const res = await fetch('http://localhost:4000/db');
+        const res = await fetch(
+            'https://salty-everglades-67914.herokuapp.com/db',
+        );
         const data = await res.json(res);
         console.log(`Data fetched. Count: ${data.results}`);
 
@@ -27,45 +29,61 @@ class Index extends React.Component {
     }
 
     render() {
-        function classNames(classes){
+        function classNames(classes) {
             return Object.entries(classes)
-            .filter(([key, value]) => value)
-            .map(([key, value]) => key)
-            .join(' ');
+                .filter(([key, value]) => value)
+                .map(([key, value]) => key)
+                .join(' ');
         }
-        var openClasses = classNames({openButton: true, activeButton: this.state.open})
+        var openClasses = classNames({
+            openButton: true,
+            activeButton: this.state.open,
+        });
         return (
-            <div class = "wrapper background">
+            <div class="wrapper background">
                 <div className="body">
-                    <Head>
-                    </Head>
-                        <nav>
-                        <li><a href="/"> Home </a>
+                    <Head />
+                    <nav>
+                        <li>
+                            <a href="/"> Home </a>
                         </li>
                     </nav>
-                    <center><h1> New Grad </h1></center>
-                    <center><p> New Grad is a ___</p></center>
-                    <div class= "wrapper">
-                    <div class= "inner button">
-                    <button class= {openClasses} onClick={this.toggleOpen.bind(this)}>Open</button>
+                    <center>
+                        <h1> New Grad </h1>
+                    </center>
+                    <center>
+                        <p> New Grad is a ___</p>
+                    </center>
+                    <div class="wrapper">
+                        <div class="inner button">
+                            <button
+                                class={openClasses}
+                                onClick={this.toggleOpen.bind(this)}>
+                                Open
+                            </button>
+                        </div>
                     </div>
-                    </div>
-            <div class = "wrapper">
-             <div class = "container inner">
-                        {this.props.companies.map(company => {
-                            if (!company.status && !this.state.open) {
-                                return;
-                            }
-                            var companyClasses = classNames({
-                                'company-card': true,
-                                'open' : company.status
-                            })
-                            return (
-                                <div className = {companyClasses} key={company.id}>
-                                    <Company key={company.id} company={company}/>
-                                </div>
-                            );
-                        })}
+                    <div class="wrapper">
+                        <div class="container inner">
+                            {this.props.companies.map(company => {
+                                if (company.status == '0' && !this.state.open) {
+                                    return;
+                                }
+                                var companyClasses = classNames({
+                                    'company-card': true,
+                                    open: company.status,
+                                });
+                                return (
+                                    <div
+                                        className={companyClasses}
+                                        key={company.id}>
+                                        <Company
+                                            key={company.id}
+                                            company={company}
+                                        />
+                                    </div>
+                                );
+                            })}
                         </div>
                     </div>
                 </div>
